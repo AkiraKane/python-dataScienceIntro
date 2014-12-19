dfa833890a216716da21f9ab2b00f7f0e41579fd
Benjamin Wulf <wulf.benji@gmail.com>
2014-12-19 11:50:41 -0500

    sqlite chapter

---
+++ panda0.py
@@ -0,0 +1,8 @@
+# Pandas
+import pandas as pd
+
+input_dataframe = pd.read_csv('lecz-urban-rural-population-land-area-estimates-v2-csv/lecz-urban-rural-population-land-area-estimates_continent-90m.csv')
+
+input_dataframe['Continent']
+
+input_dataframe[0:10]
